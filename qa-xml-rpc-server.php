6c855d68ddd12b10e45de1160967830ed9c69edd
NoahY <noahy57@gmail.com>
2013-05-24 09:05:32 -0500

    new question getting

--- qa-xml-rpc-server.php
+++ qa-xml-rpc-server.php
@@ -268,17 +268,10 @@ class q2a_xmlrpc_server extends IXR_Server {
 
 	function get_single_question($data, $questionid) {
 		$userid = qa_get_logged_in_userid();
-		$options=qa_post_html_defaults('Q', @$data['full']);
-		if (isset($data['categorypathprefix']))
-			$options['categorypathprefix'] = $categorypathprefix;
-
-		$cookieid=qa_cookie_get();
 			
 		if(@$data['full']) {
 			
-			$coptions=qa_post_html_defaults('C', true);
-
-			@list($questionin, $childposts, $achildposts, $parentquestion, $closepost, $extravalue, $categories, $favorite)=qa_db_select_with_pending(
+			@list($question, $childposts, $achildposts, $parentquestion, $closepost, $extravalue, $categories, $favorite)=qa_db_select_with_pending(
 				qa_db_full_post_selectspec($userid, $questionid),
 				qa_db_full_child_posts_selectspec($userid, $questionid),
 				qa_db_full_a_child_posts_selectspec($userid, $questionid),
@@ -288,35 +281,29 @@ class q2a_xmlrpc_server extends IXR_Server {
 				qa_db_category_nav_selectspec($questionid, true, true, true),
 				isset($userid) ? qa_db_is_favorite_selectspec($userid, QA_ENTITY_QUESTION, $questionid) : null
 			);
-
-
-			$answers = array();
-			$allcomments = array();
 			
-			foreach ($childposts as $postid => $post)
-				switch ($post['type']) {
-					case 'A':
-					case 'A_HIDDEN':
-					case 'A_QUEUED':
-						$answers[]=$post;
-						break;
-					case 'Q': // never show follow-on Qs which have been hidden, even to admins
-					case 'C':
-					case 'C_HIDDEN':
-					case 'C_QUEUED':
-						$allcomments[] = $post;
-						break;
-				}
+			if ($question['basetype']!='Q') // don't allow direct viewing of other types of post
+				return null;
 
-			foreach ($achildposts as $postid => $post)
-				switch ($post['type']) {
-					case 'Q': // never show follow-on Qs which have been hidden, even to admins
-					case 'C':
-					case 'C_HIDDEN':
-					case 'C_QUEUED':
-						$allcomments[] = $post;
-						break;
-				}
+			$question['extra']=$extravalue;
+			
+			$answers=qa_page_q_load_as($question, $childposts);
+			$allcomments=qa_page_q_load_c_follows($question, $childposts, $achildposts);
+			
+			$question=$question+qa_page_q_post_rules($question, null, null, $childposts); // array union
+			
+			if ($question['selchildid'] && (@$answers[$question['selchildid']]['type']!='A'))
+				$question['selchildid']=null; // if selected answer is hidden or somehow not there, consider it not selected
+
+			foreach ($answers as $key => $answer) {
+				$answers[$key]=$answer+qa_page_q_post_rules($answer, $question, $answers, $achildposts);
+				$answers[$key]['isselected']=($answer['postid']==$question['selchildid']);
+			}
+
+			foreach ($allcomments as $key => $commentfollow) {
+				$parent=($commentfollow['parentid']==$questionid) ? $question : @$answers[$commentfollow['parentid']];
+				$allcomments[$key]=$commentfollow+qa_page_q_post_rules($commentfollow, $parent, $allcomments, null);
+			}
 
 			$usershtml=qa_userids_handles_html(array_merge(array($questionin), $answers, $allcomments), true);
 			
@@ -651,6 +638,10 @@ class q2a_xmlrpc_server extends IXR_Server {
 		$output['confirmation'] = $error;
 		$output['message'] = $error?qa_lang( 'xmlrpc/error' ):qa_lang( 'xmlrpc/success' );
 		return $output;
+	}
+
+	function get_post_buttons($post) {
+		
 	}
 
 	function get_post_avatar($post) {
